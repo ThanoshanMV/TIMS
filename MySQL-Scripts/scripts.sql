bca6f781b9207184e683bf30756f1311b39d3228
ThanoshanMV <mvthanoshan9@gmail.com>
2020-11-24 14:01:12 +0530

    Adding MySQL Scripts directory

---
+++ MySQL-Scripts/scripts.sql
@@ -0,0 +1,82 @@
+# Creating database tims
+CREATE DATABASE tims;
+
+# use the tims database
+USE tims;
+
+# create table role
+CREATE TABLE role (
+	id INT NOT NULL,
+    rolename VARCHAR(20),
+    
+    PRIMARY KEY (id)
+);
+
+# create table employee
+CREATE TABLE employee (
+	id INT NOT NULL AUTO_INCREMENT,
+    roleid INT,
+    name VARCHAR(255),
+    username VARCHAR(255),
+    nic VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    job VARCHAR(255),
+    password VARCHAR(255),
+    
+    PRIMARY KEY (id),
+    FOREIGN KEY (roleid) REFERENCES role(id)
+);
+
+# create table payment
+CREATE TABLE payment (
+	id INT NOT NULL,
+	name VARCHAR(255),
+	nic VARCHAR(255) NOT NULL,
+    park VARCHAR(255),
+    # decimal is an exact fixed-point number
+    # total number of digits = 15, number of digits after decimal point = 2
+    totalpayment DECIMAL(15,2),
+    year2013 DECIMAL(15,2),
+    year2014 DECIMAL(15,2),
+    year2015 DECIMAL(15,2),
+    year2016 DECIMAL(15,2),
+    year2017 DECIMAL(15,2),
+    year2018 DECIMAL(15,2),
+    year2019 DECIMAL(15,2),
+    year2020 DECIMAL(15,2),
+    year2021 DECIMAL(15,2),
+    year2022 DECIMAL(15,2),
+    
+    PRIMARY KEY (id)
+);
+
+# create table driver
+CREATE TABLE driver (
+	id INT NOT NULL AUTO_INCREMENT,
+    paymentid INT,
+	name VARCHAR(255),
+	nic VARCHAR(255) NOT NULL,
+    phoneno INT,
+    wheelno VARCHAR(255),
+    address VARCHAR(255),
+    parkno VARCHAR(255),
+	park VARCHAR(10),
+    images BLOB,
+    imageurl VARCHAR(255),
+    gs VARCHAR(10),
+    
+    PRIMARY KEY (id),
+    FOREIGN KEY (paymentid) REFERENCES payment(id)
+);
+
+# create table employee_driver
+CREATE TABLE employee_driver (
+	employeeid INT NOT NULL,
+    driverid INT NOT NULL,
+    
+    PRIMARY KEY (employeeid, driverid),
+    FOREIGN KEY (employeeid) REFERENCES employee(id),
+    FOREIGN KEY (driverid) REFERENCES driver(id)
+);
+
+DROP DATABASE tims;
