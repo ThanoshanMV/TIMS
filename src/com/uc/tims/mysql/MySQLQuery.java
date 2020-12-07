cfbf9a9ce07eb350ec602ff9965bf79113690465
ThanoshanMV <mvthanoshan9@gmail.com>
2020-12-07 15:31:35 +0530

    added queries as final and removed unnecessary getters, setters

--- src/com/uc/tims/mysql/MySQLQuery.java
+++ src/com/uc/tims/mysql/MySQLQuery.java
@@ -5,161 +5,42 @@ public class MySQLQuery {
 	/**
 	 * Queries for admin table
 	 */
-	private static String sqlQueryForAdminLogIn = "SELECT * FROM `employee` WHERE `username`= ? AND `password`= ?";
+	private final static String sqlQueryForAdminLogIn = "SELECT * FROM `employee` WHERE `username`= ? AND `password`= ?";
 	
 	/**
 	 * Queries for employee table
 	 */
-	private static String sqlQueryForUserRegistration = "INSERT INTO `employee`(`roleid`,`name`,`username`,`nic`,`job`,`password`) VALUES (?,?,?,?,?,?)";
+	private final static String sqlQueryForUserRegistration = "INSERT INTO `employee`(`roleid`,`name`,`username`,`nic`,`job`,`password`) VALUES (?,?,?,?,?,?)";
 	
-	private static String sqlQueryForUserLogIn = "SELECT * FROM `employee` WHERE `username`= ? AND `password`= ?";
+	private final static String sqlQueryForUserLogIn = "SELECT * FROM `employee` WHERE `username`= ? AND `password`= ?";
 	
 	
 	/**
 	 * Queries for driver table 
 	 */
-	private static String sqlQueryForDriverRegistration = "INSERT INTO `driver`(`name`,`nic`,`phoneno`,`wheelno`,`address`,`parkno`,`park`,`images`,`imageurl`,`gs`) VALUES (?,?,?,?,?,?,?,?,?,?)";
+	private final static String sqlQueryForDriverRegistration = "INSERT INTO `driver`(`name`,`nic`,`phoneno`,`wheelno`,`address`,`parkno`,`park`,`images`,`imageurl`,`gs`) VALUES (?,?,?,?,?,?,?,?,?,?)";
 	
-	private static String sqlQueryForUpdatePaymentidInDriver = "UPDATE `driver` SET `paymentid` = ? WHERE id = ?";
+	private final static String sqlQueryForUpdatePaymentidInDriver = "UPDATE `driver` SET `paymentid` = ? WHERE id = ?";
 	
-	private static String sqlQueryForSelectDriverByID = "SELECT `id` FROM `driver` WHERE `nic` = ?";
+	private final static String sqlQueryForSelectDriverByID = "SELECT `id` FROM `driver` WHERE `nic` = ?";
 
-	private static String sqlQueryForChooseDriverToEdit = "SELECT * FROM `driver` WHERE `park`= ? AND `nic`= ?";
+	private final static String sqlQueryForChooseDriverToEdit = "SELECT * FROM `driver` WHERE `park`= ? AND `nic`= ?";
 	
-	private static String sqlQueryForSelectDriverDetailsForSearchOperartion = "SELECT * FROM `driver` WHERE `parkno`= ?";
+	private final static String sqlQueryForSelectDriverDetailsForSearchOperartion = "SELECT * FROM `driver` WHERE `parkno`= ?";
 	
-	private static String sqlQueryForSelectDriverDetailsBySearch = "SELECT `park`,`parkno`,`wheelno`,`name`,`address`,`nic`,`phoneno`,`gs` FROM `driver` WHERE ? LIKE ?";
+	private final static String sqlQueryForSelectDriverDetailsBySearch = "SELECT `park`,`parkno`,`wheelno`,`name`,`address`,`nic`,`phoneno`,`gs` FROM `driver` WHERE ? LIKE ?";
 	
-	private static String sqlQueryForDeleteDriverByNic = "DELETE FROM `driver` WHERE `nic` = ?";
+	private final static String sqlQueryForDeleteDriverByNic = "DELETE FROM `driver` WHERE `nic` = ?";
 	
-	private static String sqlQueryForCountBy = "SELECT COUNT(`address`) FROM `driver` WHERE ? = ?";
+	private final static String sqlQueryForCountBy = "SELECT COUNT(`address`) FROM `driver` WHERE ? = ?";
 	
-	private static String sqlQueryForGetSummaryByParkCount = "Select park as Park, count(*) as Total from driver Group by park";
+	private final static String sqlQueryForGetSummaryByParkCount = "Select park as Park, count(*) as Total from driver Group by park";
 
 	/**
 	 * Queries for payment table 
 	 */
-	private static String sqlQueryForInsertPaymentRow = "INSERT INTO `payment`(`id`,`name`,`nic`,`park`,`totalPayment`,`year2013`,`year2014`,`year2015`,`year2016`,`year2017`,`year2018`,`year2019`,`year2020`,`year2021`,`year2022`) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+	private final static String sqlQueryForInsertPaymentRow = "INSERT INTO `payment`(`id`,`name`,`nic`,`park`,`totalPayment`,`year2013`,`year2014`,`year2015`,`year2016`,`year2017`,`year2018`,`year2019`,`year2020`,`year2021`,`year2022`) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
 
-	private static String sqlQueryForDeletePaymentByNic = "DELETE FROM `payment` WHERE `nic` = ?";
-	
-	
-	
-	public static String getSqlQueryForAdminLogIn() {
-		return sqlQueryForAdminLogIn;
-	}
+	private final static String sqlQueryForDeletePaymentByNic = "DELETE FROM `payment` WHERE `nic` = ?";
 
-	public static void setSqlQueryForAdminLogIn(String sqlQueryForAdminLogIn) {
-		MySQLQuery.sqlQueryForAdminLogIn = sqlQueryForAdminLogIn;
-	}
-
-	public static String getSqlQueryForUserRegistration() {
-		return sqlQueryForUserRegistration;
-	}
-
-	public static void setSqlQueryForUserRegistration(String sqlQueryForUserRegistration) {
-		MySQLQuery.sqlQueryForUserRegistration = sqlQueryForUserRegistration;
-	}
-
-	public static String getSqlQueryForUserLogIn() {
-		return sqlQueryForUserLogIn;
-	}
-
-	public static void setSqlQueryForUserLogIn(String sqlQueryForUserLogIn) {
-		MySQLQuery.sqlQueryForUserLogIn = sqlQueryForUserLogIn;
-	}
-
-	public static String getSqlQueryForDriverRegistration() {
-		return sqlQueryForDriverRegistration;
-	}
-
-	public static void setSqlQueryForDriverRegistration(String sqlQueryForDriverRegistration) {
-		MySQLQuery.sqlQueryForDriverRegistration = sqlQueryForDriverRegistration;
-	}
-
-	public static String getSqlQueryForUpdatePaymentidInDriver() {
-		return sqlQueryForUpdatePaymentidInDriver;
-	}
-
-	public static void setSqlQueryForUpdatePaymentidInDriver(String sqlQueryForUpdatePaymentidInDriver) {
-		MySQLQuery.sqlQueryForUpdatePaymentidInDriver = sqlQueryForUpdatePaymentidInDriver;
-	}
-
-	public static String getSqlQueryForSelectDriverByID() {
-		return sqlQueryForSelectDriverByID;
-	}
-
-	public static void setSqlQueryForSelectDriverByID(String sqlQueryForSelectDriverByID) {
-		MySQLQuery.sqlQueryForSelectDriverByID = sqlQueryForSelectDriverByID;
-	}
-
-	public static String getSqlQueryForChooseDriverToEdit() {
-		return sqlQueryForChooseDriverToEdit;
-	}
-
-	public static void setSqlQueryForChooseDriverToEdit(String sqlQueryForChooseDriverToEdit) {
-		MySQLQuery.sqlQueryForChooseDriverToEdit = sqlQueryForChooseDriverToEdit;
-	}
-
-	public static String getSqlQueryForInsertPaymentRow() {
-		return sqlQueryForInsertPaymentRow;
-	}
-
-	public static void setSqlQueryForInsertPaymentRow(String sqlQueryForInsertPaymentRow) {
-		MySQLQuery.sqlQueryForInsertPaymentRow = sqlQueryForInsertPaymentRow;
-	}
-
-	public static String getSqlQueryForSelectDriverDetailsForSearchOperartion() {
-		return sqlQueryForSelectDriverDetailsForSearchOperartion;
-	}
-
-	public static void setSqlQueryForSelectDriverDetailsForSearchOperartion(
-			String sqlQueryForSelectDriverDetailsForSearchOperartion) {
-		MySQLQuery.sqlQueryForSelectDriverDetailsForSearchOperartion = sqlQueryForSelectDriverDetailsForSearchOperartion;
-	}
-
-	public static String getSqlQueryForSelectDriverDetailsBySearch() {
-		return sqlQueryForSelectDriverDetailsBySearch;
-	}
-
-	public static void setSqlQueryForSelectDriverDetailsBySearch(String sqlQueryForSelectDriverDetailsBySearch) {
-		MySQLQuery.sqlQueryForSelectDriverDetailsBySearch = sqlQueryForSelectDriverDetailsBySearch;
-	}
-
-	public static String getSqlQueryForDeleteDriverByNic() {
-		return sqlQueryForDeleteDriverByNic;
-	}
-
-	public static void setSqlQueryForDeleteDriverByParkno(String sqlQueryForDeleteDriverByNic) {
-		MySQLQuery.sqlQueryForDeleteDriverByNic = sqlQueryForDeleteDriverByNic;
-	}
-
-	public static String getSqlQueryForCountBy() {
-		return sqlQueryForCountBy;
-	}
-
-	public static void setSqlQueryForCountBy(String sqlQueryForCountBy) {
-		MySQLQuery.sqlQueryForCountBy = sqlQueryForCountBy;
-	}
-
-	public static String getSqlQueryForDeletePaymentByNic() {
-		return sqlQueryForDeletePaymentByNic;
-	}
-
-	public static void setSqlQueryForDeletePaymentByNic(String sqlQueryForDeletePaymentByNic) {
-		MySQLQuery.sqlQueryForDeletePaymentByNic = sqlQueryForDeletePaymentByNic;
-	}
-
-	public static String getSqlQueryForGetSummaryByParkCount() {
-		return sqlQueryForGetSummaryByParkCount;
-	}
-
-	public static void setSqlQueryForGetSummaryByParkCount(String sqlQueryForGetSummaryByParkCount) {
-		MySQLQuery.sqlQueryForGetSummaryByParkCount = sqlQueryForGetSummaryByParkCount;
-	}
-
-	public static void setSqlQueryForDeleteDriverByNic(String sqlQueryForDeleteDriverByNic) {
-		MySQLQuery.sqlQueryForDeleteDriverByNic = sqlQueryForDeleteDriverByNic;
-	}
-	
 }
